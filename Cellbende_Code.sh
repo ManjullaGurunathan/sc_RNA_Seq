67ecf2a13c0ebbdbffbad0713af356c239bff4b0
Manjula Gurunathan <manjulagurunathan27@gmail.com>
2025-12-01 13:04:29 +0530

    Add files via upload

---
+++ Cellbende_Code.sh
@@ -0,0 +1,4 @@
+cellbender remove-background \
+  --cuda \
+  --input /Users/manjulagurunathan/Downloads/E-MTAB-7895-quantification-raw-files/E-MTAB-7895_combined.h5ad \
+  --output /Users/manjulagurunathan/Downloads/E-MTAB-7895-quantification-raw-files/E-MTAB-7895_cellbender_output_file.h5
---
+++ Cellbender_input_h5ad.py
@@ -0,0 +1,18 @@
+import scanpy as sc
+import pandas as pd
+
+folder_path = '/Users/manjulagurunathan/Downloads/E-MTAB-7895-quantification-raw-files/'
+
+
+adata = sc.read_mtx(f"{folder_path}/matrix.mtx").T
+genes = pd.read_csv(f"{folder_path}/features.csv", header=None)
+barcodes = pd.read_csv(f"{folder_path}/barcodes.csv", header=None)
+
+adata.var_names = genes[0]
+adata.obs_names = barcodes[0]
+
+print(f"adata shape: {adata.shape}")
+print(f"First 5 genes: {list(adata.var_names)[:5]}")
+print(f"First 5 barcodes: {list(adata.obs_names)[:5]}")
+
+adata.write("/Users/manjulagurunathan/Downloads/E-MTAB-7895-quantification-raw-files/E-MTAB-7895_combined.h5ad")
