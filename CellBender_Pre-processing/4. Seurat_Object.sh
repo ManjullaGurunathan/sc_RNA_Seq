db70ed7e045f8b242560c25cf4338c8f7f8653b3
Manjula Gurunathan <manjulagurunathan27@gmail.com>
2025-12-01 13:13:57 +0530

    Add header comment for Seurat conversion script

--- CellBender_Pre-processing/4. Seurat_Object.sh
+++ CellBender_Pre-processing/4. Seurat_Object.sh
@@ -1,4 +1,5 @@
+#Convert CellBender output to Seurat-compatible format
+
 pip install tables
 
-#Convert CellBender output to Seurat-compatible format
 ptrepack --complevel 5 E-MTAB-7895_cellbender_output_file_3_filtered.h5:/matrix E-MTAB-7895_cellbender_output_filtered_seurat.h5:/matrix
