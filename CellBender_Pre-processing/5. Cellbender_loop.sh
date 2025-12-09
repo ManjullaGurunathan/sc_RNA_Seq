e4c645fc02070dd263b5810fa1446da3b68a8a65
Manjula Gurunathan <manjulagurunathan27@gmail.com>
2025-12-09 10:00:08 +0530

    Update input and output directory paths in script

--- CellBender_Pre-processing/5. Cellbender_loop.sh
+++ CellBender_Pre-processing/5. Cellbender_loop.sh
@@ -3,13 +3,13 @@
 #PBS -l walltime=01:00:00
 
 
-INPUT_DIR="/Users/manjulagurunathan/Downloads/E-MTAB-7895-quantification-raw-files"
-OUTPUT_DIR="/Users/manjulagurunathan/Downloads/E-MTAB-7895-quantification-raw-files/CellBender_outs/cellbender_out"
+INPUT_DIR="/path/to/directory/with/input"
+OUTPUT_DIR="/path/to/desired/output/directory"
 mkdir -p "$OUTPUT_DIR"
 
-for h5ad_file in "$INPUT_DIR"/*.h5ad; do
-    if [[ -f "$h5ad_file" ]]; then
-        base_name=$(basename "$h5ad_file" .h5ad)
+for h5_file in "$INPUT_DIR"/*.h5; do
+    if [[ -f "$h5_file" ]]; then
+        base_name=$(basename "$h5_file" .h5)
         sample_dir="$OUTPUT_DIR/${base_name}_cellbender_outs"
         mkdir -p "$sample_dir"
 
@@ -18,7 +18,7 @@ for h5ad_file in "$INPUT_DIR"/*.h5ad; do
         echo "Running Cellbender, $basename -> ${basename}_cellbender.h5"
         #cellbender remove-background \
             #--cuda \
-            #--input "$h5ad_file" \
+            #--input "$h5_file" \
             #--output "$output_file" 
         echo "Cellbender analysis completed on $base_name, Ambient RNA removed."
     fi
